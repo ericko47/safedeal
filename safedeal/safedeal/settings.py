8ba56e149641d969dc39e107585a87613bbb98df
Erick Omondi <69643266+ericko47@users.noreply.github.com>
2025-04-13 20:50:52 +0300

    Update settings.py

--- safedeal/safedeal/settings.py
+++ safedeal/safedeal/settings.py
@@ -88,10 +88,8 @@ WSGI_APPLICATION = 'safedeal.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {'ENGINE': 'django.db.backends.sqlite3','NAME': BASE_DIR / 'db.sqlite3',}
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
