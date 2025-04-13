01e540d30fa370666af00302602d8cf1ab630061
Erick Omondi <69643266+ericko47@users.noreply.github.com>
2025-04-13 19:56:45 +0300

    Update settings.py

--- safedeal/safedeal/settings.py
+++ safedeal/safedeal/settings.py
@@ -174,6 +174,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')    # For prod
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
