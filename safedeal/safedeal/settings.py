84aca318a18b7d70d33443bf22667da6d0673a20
Erick <erickogutu47@gmail.com>
2025-04-16 20:51:56 +0300

    Make national_id field optional and update database settings

--- safedeal/safedeal/settings.py
+++ safedeal/safedeal/settings.py
@@ -95,8 +95,11 @@ WSGI_APPLICATION = 'safedeal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {'ENGINE': 'django.db.backends.sqlite3','NAME': BASE_DIR / 'db.sqlite3',}
+}
 # DATABASES = {
-#     'default': {'ENGINE': 'django.db.backends.sqlite3','NAME': BASE_DIR / 'db.sqlite3',}
+#     'default': dj_database_url.config(conn_max_age=600)
 # }
 
 
