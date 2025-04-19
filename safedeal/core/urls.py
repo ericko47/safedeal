aa21b0fe7545c85d1f20b30a2dad682edc7ceb1b
Erick <erickogutu47@gmail.com>
2025-04-19 12:48:51 +0300

    Add change password functionality and update profile links

--- safedeal/core/urls.py
+++ safedeal/core/urls.py
@@ -51,4 +51,11 @@ urlpatterns = [
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+   
+
+
+    path('change-password/', auth_views.PasswordChangeView.as_view(template_name='accounts/change_password.html'), name='change_password'),
+    path('change-password/done/', auth_views.PasswordChangeDoneView.as_view(template_name='accounts/change_password_done.html'), name='password_change_done'),
+
+
 ]
